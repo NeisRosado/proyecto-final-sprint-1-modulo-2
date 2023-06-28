1f3024c1ff77c8359ffecb8afddabbe062b78681
Neis Rosado <neis.study@gmail.com>
2023-06-28 02:10:01 -0500

    fix signUp function

--- src/app/scripts/UI/domElements.js
+++ src/app/scripts/UI/domElements.js
@@ -1,4 +1,26 @@
 export const form_login = document.getElementById('form_login')
 export const signUpbtn= document.getElementById("signUpbtn"); 
 export const registrationFormContainer = document.getElementById('registration-form-container');
-export const form_btn = document.getElementById("form_btn")
+export const getFormValues = () => ({
+    name: getFormValue('name'),
+    phoneNumber: getFormValue('phone_number'),
+    password: getFormValue('password'),
+    profilePicUrl: getFormValue('profile_pic_url'),
+    about: getFormValue('phrase')
+  });
+  
+export const getFormValue = (fieldId) => {
+    const field = document.getElementById(fieldId);
+    if (field) {
+      return field.value.trim();
+    }
+    return '';
+  };
+  
+  export const clearForm = () => {
+    const form = document.querySelector('.form');
+    if (form) {
+      form.reset();
+    }
+  };
+  
--- src/app/scripts/services/signUp.js
+++ src/app/scripts/services/signUp.js
@@ -1,62 +1,66 @@
-import axios from "axios";
 import Swal from 'sweetalert';
-import { registrationFormContainer } from "../UI/domElements.js";
+import { getFormValues, clearForm } from '../UI/domElements.js'; 
 import { endpoints } from "./data.js";
+import axios from 'axios';
+import { registrationFormContainer } from '../UI/domElements.js';
 
 export const handleSignUp = async (event) => {
   event.preventDefault();
 
-  const nameInput = document.getElementById("name");
-  const numberInput = document.getElementById("number");
-  const passwordInput = document.getElementById("password");
-  const imageUrlInput = document.getElementById("image-url");
-  const phraseInput = document.getElementById("phrase");
+  // Obteniene los valores de los campos del formulario
+  const { name, phoneNumber, password, profilePicUrl, about } = getFormValues();
 
-  const name = nameInput.value;
-  const number = numberInput.value;
-  const password = passwordInput.value;
-  const imageUrl = imageUrlInput.value;
-  const phrase = phraseInput.value;
-
-  if (password.length === 0 && number.length === 0) {
+  // Valida que todos los campos sean obligatorios
+  if ([name, phoneNumber, password, profilePicUrl, about].some(field => field === '')) {
     Swal('Todos los campos son obligatorios');
     return;
-  } else {
-    if (number.length === 0) {
-      Swal('El campo del número está vacío');
-      return;
-    };
-    if (password.length === 0) {
-      Swal('El campo de la contraseña está vacío');
-      return;
-    }
   }
+
+  // Verifica si el número de celular ya existe en la lista de usuarios
+  const response = await axios.get(endpoints.urlUsers);
+  const users = response.data;
+  const existingUser = users.find(user => user.phone_number === phoneNumber);
+  if (existingUser) {
+    Swal('El número de celular ingresado ya está registrado');
+    return;
+  }
+
+  // Crea el objeto de usuario
+  const newUser = {
+    id: generateUserId(users), // Genera el ID secuencialmente
+    name,
+    phone_number: phoneNumber,
+    password,
+    profile_pic_url: profilePicUrl,
+    is_online: false,
+    about,
+    last_time: new Date().toISOString()
+  };
+
+  // Realiza la petición POST para crear el nuevo usuario
 
   try {
-    const response = await axios.get(endpoints.urlUsers);
-    const users = response.data;
-    const numberExists = users.some(user => user.phone_number === number);
-
-    if (numberExists) {
-      Swal('El número de celular ingresado ya está registrado.');
-    } else {
-      const newUser = {
-        name: name,
-        phone_number: number,
-        password: password,
-        image_url: imageUrl,
-        phrase: phrase
-      };
-
-      await axios.post(endpoints.urlUsers, newUser);
-      Swal('El nuevo usuario ha sido creado exitosamente.');
-    }
+    await axios.post(endpoints.urlUsers, newUser);
+    Swal('El nuevo usuario fue creado exitosamente').then(() => {
+      clearForm(); // Limpiar el formulario después de mostrar la alerta
+      window.location.href = 'index.html'; // Redirige a la página de inicio de sesión
+    });
   } catch (error) {
-    console.error(error);
-    Swal('Ocurrió un error al crear el nuevo usuario. Por favor, intenta nuevamente.');
+    Swal('Ocurrió un error al crear el usuario');
   }
-}
+};
 
+// Genera el ID secuencialmente
+const generateUserId = (users) => {
+  let maxId = 0;
+  users.forEach(user => {
+    const userId = parseInt(user.id);
+    if (userId > maxId) {
+      maxId = userId;
+    }
+  });
+  return (maxId + 1).toString();
+};
+
+// click para registrar 
 registrationFormContainer.addEventListener('submit', handleSignUp);
-
-
