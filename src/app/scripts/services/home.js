95ee682d609ac6d98c7d61942218380563b0a691
Neis Rosado <neis.study@gmail.com>
2023-06-29 17:10:49 -0500

    changes in home.js

--- src/app/scripts/services/home.js
+++ src/app/scripts/services/home.js
@@ -1,16 +1,15 @@
 import axios from "axios";
-import { chatUserImage } from "../UI/domElements.js";
 import { endpoints } from "./data.js";
+import { chatUserImage } from "../UI/domElements.js";
 
-export const updateUserImage = async (loggedInUserId) => {
+export const updateUserImage = async () => {
   try {
     const response = await axios.get(endpoints.urlUsers);
     const users = response.data;
+    const loggedInUserId = 2; // ID del usuario que inició sesión (actualízalo según tu lógica de inicio de sesión)
     const loggedInUser = users.find(user => user.id === loggedInUserId);
     if (loggedInUser) {
       chatUserImage.src = loggedInUser.profile_pic_url;
-    } else {
-      console.error("El usuario no ha iniciado sesión correctamente.");
     }
   } catch (error) {
     console.error("Error al obtener los datos de los usuarios:", error);
--- src/app/scripts/services/login.js
+++ src/app/scripts/services/login.js
@@ -3,20 +3,18 @@ import Swal from 'sweetalert';
 import { form_login } from "../UI/domElements.js"
 import { endpoints } from "./data.js";
 import { showChatView } from "../UI/showViews.js";
-
-
 export const login = async (event) => {
     event.preventDefault();
-    const {number, password} = event.target;
-    if(password.value.length === 0 && number.value.length === 0) {
+    const { number, password } = event.target;
+    if (password.value.length === 0 && number.value.length === 0) {
         Swal('Todos los campos son obligatorios');
         return;
     } else {
-        if(number.value.length === 0 ) {
+        if (number.value.length === 0) {
             Swal('El campo del número está vacío');
             return;
         };
-        if(password.value.length === 0) {
+        if (password.value.length === 0) {
             Swal('El campo de la contraseña está vacío');
             return;
         }
@@ -26,21 +24,19 @@ export const login = async (event) => {
     const users = response.data;
     let numberExist = false;
     users.forEach(user => {
-        if(user.phone_number === number.value)
-        {
+        if (user.phone_number === number.value) {
             numberExist = true;
-            if(user.password === password.value)
-            {
+            if (user.password === password.value) {
                 Swal(`Bienvenido ${user.name}`);
                 showChatView();
                 return;
-            } else{
+            } else {
                 Swal('La contraseña ingresada es incorrecta');
                 return;
             }
         }
     });
-    if(numberExist === false){
+    if (numberExist === false) {
         Swal('El número no existe');
     }
 }
