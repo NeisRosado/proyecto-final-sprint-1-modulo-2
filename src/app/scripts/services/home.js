4e7f623f1d02e97aed043178420c0aed5a894046
Neis Rosado <neis.study@gmail.com>
2023-06-30 19:48:27 -0500

    fix html

--- src/app/scripts/services/home.js
+++ src/app/scripts/services/home.js
@@ -3,17 +3,17 @@ import { endpoints } from "./data.js";
 import { chatUserImage } from "../UI/domElements.js";
 import { getChatHistory } from "./chatHistory.js";
 
-//  export const updateUserImage = async (loggedInUserId) => {
-//   try {
-//     const response = await axios.get(endpoints.urlUsers);
-//     const users = response.data;
-//     const loggedInUser = users.find(user => user.id === loggedInUserId);
-//     if (loggedInUser) {
-//       chatUserImage.src = loggedInUser.profile_pic_url;
-//     }
-//   } catch (error) {
-//     console.error("Error al obtener los datos de los usuarios:", error);
-//   }
-// };
+ export const updateUserImage = async (loggedInUserId) => {
+  try {
+    const response = await axios.get(endpoints.urlUsers);
+    const users = response.data;
+    const loggedInUser = users.find(user => user.id === loggedInUserId);
+    if (loggedInUser) {
+      chatUserImage.src = loggedInUser.profile_pic_url;
+    }
+  } catch (error) {
+    console.error("Error al obtener los datos de los usuarios:", error);
+  }
+};
 
 getChatHistory();
