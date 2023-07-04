675b27674e1ae35f25acd4bede2a0532d02f34bc
AnaRamirez12 <anamaria103458@gmail.com>
2023-07-03 22:17:14 -0500

    Agregadas nuevas funciones

--- src/app/scripts/services/home.js
+++ src/app/scripts/services/home.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { endpoints } from "./data.js";
-import { chatUserImage } from "../UI/domElements.js";
+import { chatUserImage, profileCurrentName } from "../UI/domElements.js";
 import { getChatHistory } from "./chatHistory.js";
+import { updateDataByAttribute } from "./profile.js";
 
  export const updateUserImage = async (loggedInUserId) => {
   try {
@@ -15,5 +16,19 @@ import { getChatHistory } from "./chatHistory.js";
     console.error("Error al obtener los datos de los usuarios:", error);
   }
 };
+
+export const setCurrentUser = (user) => {
+  updateDataByAttribute("name", user.name);
+  updateDataByAttribute("url_picture", user.profile_pic_url);
+  localStorage.setItem('current_user', JSON.stringify(user))
+}
+
+export const getCurrentUser = () => {
+  let currentUser = localStorage.getItem('current_user');
+  if (currentUser == null || currentUser == undefined || currentUser == "") {
+    throw new Error("No hay un usuario asignado");
+  }
+  return JSON.parse(currentUser);
+}
 
 getChatHistory();
